6e9f170a32aa6f99f087d0e92d7275e23cb5bb29
Marina Mitrovic <marina.mitrovic@bearingpoint.com>
2024-11-18 17:18:09 +0100

    Updates

--- _app/immutable/chunks/paths.D3-9UWZC.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_2ccwjc)==null?void 0:s.base)??"/die-buchclub.github.io";var c;const t=((c=globalThis.__sveltekit_2ccwjc)==null?void 0:c.assets)??e;export{t as a,e as b};
--- _app/immutable/entry/start.DwUsYc_7.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DJ1CKTtH.js";export{t as start};
