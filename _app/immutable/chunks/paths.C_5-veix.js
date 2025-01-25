6d8ddc9d51e62fc9a8c8aae22b26048f60537e5d
mia <mia.mitrovic@gmx.at>
2025-01-25 23:07:29 +0100

    Updates

--- _app/immutable/chunks/paths.C_5-veix.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_9glipj)==null?void 0:s.base)??"/die-buchclub.github.io";var e;const a=((e=globalThis.__sveltekit_9glipj)==null?void 0:e.assets)??t;export{a,t as b};
